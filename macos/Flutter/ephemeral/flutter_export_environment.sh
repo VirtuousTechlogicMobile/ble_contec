aab62f3961ba0defa235367b8dfabc878004db8d
virtoustechlogic <virtuoustechlogicmobile@gmail.com>
2024-01-15 18:53:10 +0530

    init commit

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/lenovo/Documents/sdks/3.13.7"
+export "FLUTTER_APPLICATION_PATH=/Users/lenovo/Documents/flutter_flow/sleapcare"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
